5a57a2682efa94859eb6eb0d0b755a5dfd3f9ce1
robertarnborg <robert.arnborg@gmail.com>
2019-09-21 01:31:16 +0200

    Hud Final Fantasy Theme Style :D

--- Scenes/UI/HUD.gd
+++ Scenes/UI/HUD.gd
@@ -1,9 +1,45 @@
 extends Control
 
 
-func update_HUD_timer(player_timer): ## UPDATE Timer progress bar
-	pass
+onready var timer_progress = $TimerPanel/TimeProgressBar
+onready var player_hp_current_progressbar = $PlayerPanel/HpProgressBar
+onready var player_hp_current_label = $PlayerPanel/HpProgressBar/CurrentHP
+onready var player_hp_current_max_label = $PlayerPanel/HpProgressBar/MaxHP
+onready var input_popup_menu = $InputPopUpMenu
+onready var input_attack_button = $InputPopUpMenu/InputBGPanel/AttackButton
+onready var input_dosomething_button = $InputPopUpMenu/InputBGPanel/DoSomething ### Add Functionality here... :P
+
+func _ready():
+	$InputPopUpMenu/InputBGPanel/AttackButton.connect("pressed", self,"attack_enemy")
+	inactivate_player_input()
 
 
-func update_HUD_stats(player_hp): ## Update player hp MAYBE UPDATE enemy hp...
-	pass
+func activate_player_input(): #Activate Player Input
+	input_popup_menu.show()
+	input_attack_button.disabled = false
+	input_dosomething_button.disabled = false
+
+
+func inactivate_player_input(): #Inactivate Player Input
+	input_popup_menu.hide()
+	input_attack_button.disabled = true
+	input_dosomething_button.disabled = true
+
+
+func update_HUD_timer(player_timer : int, player_timer_done : bool): #sends player timer data and sets player input true/false
+	timer_progress.value = player_timer
+	if player_timer_done:
+		activate_player_input()
+	else:
+		inactivate_player_input()
+
+
+func update_HUD_stats(player_hp, player_hp_max): ## Update player hp
+	player_hp_current_progressbar.value = player_hp
+	player_hp_current_label.text = str(player_hp)
+	player_hp_current_max_label.text = str(player_hp_max)
+
+
+func attack_enemy():
+	inactivate_player_input()
+	##CONTINUE ATTACK LOOP
