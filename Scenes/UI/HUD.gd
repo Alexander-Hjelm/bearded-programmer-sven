40ff702fbb8029a360713c167d72d338c9475411
robertarnborg <robert.arnborg@gmail.com>
2019-09-20 16:27:26 +0200

    hud_script functions in progress 1

---
+++ Scenes/UI/HUD.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func update_HUD_timer(player_timer): ## UPDATE Timer progress bar
+	pass
+
+
+func update_HUD_stats(player_hp): ## Update player hp MAYBE UPDATE enemy hp...
+	pass
