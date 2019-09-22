b9c2fb92b76c9b0ed0c7cf627aa27e8a940e015a
robertarnborg <robert.arnborg@gmail.com>
2019-09-22 22:12:52 +0200

    GameEndScene

--- Scenes/Actor/Antagonists/Bosses/GodotBoss.gd
+++ Scenes/Actor/Antagonists/Bosses/GodotBoss.gd
@@ -1,6 +1,8 @@
 extends "res://Scenes/Actor/AnimatedActor.gd"
 
 
+var game_end_screen = load("res://Scenes/UI/GameCompleted.tscn")
+
 func _init():
 	Global.godot_boss = self
 
@@ -24,3 +26,8 @@ func change_anim_state(new_state):
 			pass
 		anim_state_types.DEATH:
 			$FXMovementAnim.play("mov_death")
+
+
+func create_end_scene():
+	var end_scene_instance = game_end_screen.instance()
+	get_tree().get_root().add_child(end_scene_instance)
