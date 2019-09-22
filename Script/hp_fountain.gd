c67baf9535a19f2241bc994a29647fea49ceff4b
Alexander Hjelm <nick.nertova@gmail.com>
2019-09-22 18:19:28 +0200

    HP fountain message

--- Script/hp_fountain.gd
+++ Script/hp_fountain.gd
@@ -1,9 +1,21 @@
 extends StaticBody2D
 
-# Called when the node enters the scene tree for the first time.
+var _label_timer: Timer
+
 func _ready():
 	$Area2D.connect("body_entered", self, "_on_body_enter")
+	$Label.visible = false
+	
+	_label_timer = Timer.new()
+	_label_timer.connect("timeout",self,"_hide_label") 
+	_label_timer.one_shot = true
+	add_child(_label_timer)
 
 func _on_body_enter(body):
 	if body.name == "OverworldPlayer":
 		game_manager.restore_player_hp()
+		$Label.visible = true
+		_label_timer.start(1.0)
+
+func _hide_label():
+	$Label.visible = false
