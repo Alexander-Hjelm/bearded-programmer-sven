15d582f2c38ab539ab6d585a7717282904fa8929
robertarnborg <robert.arnborg@gmail.com>
2019-09-22 22:49:13 +0200

    IntroHowToFixed

--- TheIntroScene.gd
+++ TheIntroScene.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 var is_restart_game = false
-
+var wtf = false
 
 func _input(event):
 	if Input.is_action_just_pressed("ui_select") or Input.is_action_just_pressed("ui_accept"):
@@ -16,3 +16,13 @@ func _on_Button_button_up():
 	DaMusicManager.play_overworld_music()
 	queue_free()
 	#get_tree().change_scene("res://Overworld.tscn")
+
+
+func _on_WTFButton_pressed():
+	if wtf == false:
+		$TheCanvas/TheIntro/TheIntroAnim.play("WTF")
+		wtf = true
+		$TheCanvas/MainGame/Button/WTFButton/UISound.play()
+	else:
+		$TheCanvas/TheIntro/TheIntroAnim.play("IntroDone")
+		wtf = false
